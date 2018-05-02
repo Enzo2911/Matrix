0f24bda9e1df96e3dff0d54dc4536ac33a76e837
Enzo2911 <38391212+Enzo2911@users.noreply.github.com>
2018-05-02 16:40:46 +0200

    Add files via upload

---
+++ matrix_Aléatoire.bat
@@ -0,0 +1,14 @@
+@echo off
+
+title matrix
+
+color 02
+
+
+:matrix
+
+echo %random%%random%  %random%%random%  %random%%random%  %random%%random%  %random%%random%  %random%%random%  %random%%random%  %random%%random%  %random%%random%  %random%%random%  %random%%random%  %random%%random%  %random%%random%  %random%%random%
+
+echo %random%%random%  %random%%random%  %random%%random%  %random%%random%  %random%%random%  %random%%random%  %random%%random%  %random%%random%  %random%%random%  %random%%random%  %random%%random%  %random%%random%  %random%%random%  %random%%random%
+
+goto :matrix
